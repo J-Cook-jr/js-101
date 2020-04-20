fec022b82a2ce14e505e32c93782a3889ea23ba7
Jeff Cook <jecook2692@gmail.com>
2020-04-20 15:25:22 -0400

    adds return statement

---
+++ madlib.js
@@ -0,0 +1,7 @@
+let personName = 'Jeff';
+let schoolSubject = 'Front End Web Development';
+
+function logMessage(){
+    return(personName + "'s" , "favorite school subject is" , schoolSubject);
+}
+return(logMessage);
